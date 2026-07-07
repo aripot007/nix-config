01b205fbdc89beb94647f6b553ffc4ecaebfbe6f
aripot007 <32022176+aripot007@users.noreply.github.com>
2026-07-06 21:43:22 -0400

    chore: fmt

--- fs-diff.sh
+++ fs-diff.sh
@@ -6,17 +6,18 @@ OLD_TRANSID=$(sudo btrfs subvolume find-new "$1" 9999999)
 OLD_TRANSID=${OLD_TRANSID#transid marker was }
 
 sudo btrfs subvolume find-new "$2" "$OLD_TRANSID" |
-sed '$d' |
-cut -f17- -d' ' |
-sort |
-uniq |
-while read path; do
-  path="/$path"
-  if [ -L "$path" ]; then
-    : # The path is a symbolic link, so is probably handled by NixOS already
-  elif [ -d "$path" ]; then
-    : # The path is a directory, ignore
-  else
-    echo "$path"
-  fi
-done
+  sed '$d' |
+  cut -f17- -d' ' |
+  sort |
+  uniq |
+  while read path; do
+    path="/$path"
+    if [ -L "$path" ]; then
+      : # The path is a symbolic link, so is probably handled by NixOS already
+    elif [ -d "$path" ]; then
+      : # The path is a directory, ignore
+    else
+      echo "$path"
+    fi
+  done
+
